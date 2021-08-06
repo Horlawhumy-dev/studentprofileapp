f1280a859593ad1760d636d97f08fa04c00108bf
Arafat Olayiwola <harof.dev@gmail.com>
2021-08-06 11:52:10 +0100

    modified settings

--- studentprofile/settings.py
+++ studentprofile/settings.py
@@ -13,7 +13,7 @@ load_dotenv()
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.envirn.get('SECRET_KEY', 'django-insecure-4448=#f(vjdethbfbe&(erh6=(&@&fg4_d5mnyb!0o!^jln7rp')
+SECRET_KEY = os.getenv('SECRET_KEY', 'django-insecure-4448=#f(vjdethbfbe&(erh6=(&@&fg4_d5mnyb!0o!^jln7rp')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.getenv("YOUR_DEBUG")
